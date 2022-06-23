0e10c14f5d19e84159bcbcd81e2b2cc5bbcf541c
David Carr <dave@dcblog.dev>
2022-06-23 16:25:35 +0100

    renamed test

--- tests/Unit/DbSyncTest.php
+++ tests/Unit/DbSyncTest.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
 
 //TODO pass test
-test('demo', function () {
+test('will fail without credentials', function () {
     $this->artisan('db:production-sync')
         ->expectsOutput('DB credentials not set, have you published the config and set ENV variables?');
 });
