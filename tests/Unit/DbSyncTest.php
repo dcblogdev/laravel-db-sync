255fd6fe1bb60133c87cd5a6680cb0abab2fc4b8
David Carr <dave@dcblog.dev>
2022-06-26 17:04:41 +0100

    added test to confirm command runs and exists

--- tests/Unit/DbSyncTest.php
+++ tests/Unit/DbSyncTest.php
@@ -3,8 +3,39 @@
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
 
-//TODO pass test
-test('will fail without credentials', function () {
+test('cannot run on production', function () {
+
+    config(['app.env' => 'production']);
+
     $this->artisan('db:production-sync')
-        ->expectsOutput('DB credentials not set, have you published the config and set ENV variables?');
+        ->expectsOutput('DB sync will only run on local and staging environments')
+        ->assertExitCode(true);
+});
+
+test('can run on environment', function ($env) {
+
+    config(['app.env' => $env]);
+
+    $this->artisan('db:production-sync')
+        ->doesntExpectOutput('DB sync will only run on local and staging environments');
+})->with(['local', 'staging']);
+
+test('fails with invalid credentials', function () {
+
+    config(['app.env' => 'local']);
+
+    $this->artisan('db:production-sync')
+        ->expectsOutput('DB credentials not set, have you published the config and set ENV variables?')
+        ->assertFailed();
+});
+
+test('runs with valid credentials', function () {
+
+    config(['app.env' => 'local']);
+    config(['dbsync.host' => '127.0.0.1']);
+    config(['dbsync.username' => 'root']);
+    config(['dbsync.database' => 'demo']);
+
+    $this->artisan('db:production-sync')
+        ->expectsOutput('DB Synced');
 });
