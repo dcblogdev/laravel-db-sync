4f33220ff6fad659ad08b861d690285a53c2f88b
David Carr <dave@dcblog.dev>
2022-06-26 17:13:58 +0100

    add descriptions for config options

--- config/dbsync.php
+++ config/dbsync.php
@@ -1,9 +1,54 @@
 <?php
 
 return [
-    'host'     => env('REMOTE_DATABASE_HOST', ''),
-    'username' => env('REMOTE_DATABASE_USERNAME', ''),
-    'database' => env('REMOTE_DATABASE_NAME', ''),
-    'password' => env('REMOTE_DATABASE_PASSWORD', ''),
-    'ignore'   => env('REMOTE_DATABASE_IGNORE_TABLES', '')
+
+    /**
+     * Set true to connect over SSH
+     */
+    'useSsh' => env('REMOTE_USE_SSH', false),
+
+    /**
+     * Set the remote host either an IP address or URL
+     */
+    'host' => env('REMOTE_DATABASE_HOST', ''),
+
+    /**
+     * Set the SSH username
+     */
+    'sshUsername' => env('REMOTE_SSH_USERNAME', ''),
+
+    /**
+     * Set the SSH port number, defaults to 22 when no option provided
+     */
+    'sshPort' => env('REMOTE_DATABASE_PORT', '22'),
+
+    /**
+     * Database username
+     */
+    'username'              => env('REMOTE_DATABASE_USERNAME', ''),
+
+    /**
+     * Set the database name
+     */
+    'database'              => env('REMOTE_DATABASE_NAME', ''),
+
+    /**
+     * Set the database password
+     */
+    'password'              => env('REMOTE_DATABASE_PASSWORD', ''),
+
+    /**
+     * provide a comma seperated list of tables to ignore, when set the tables specified will not be exported
+     */
+    'ignore'                => env('REMOTE_DATABASE_IGNORE_TABLES', ''),
+
+    /**
+     * Sets if the exported SQL file will be imported into the current database connection
+     */
+    'ImportSqlFile'         => env('REMOTE_IMPORT_FILE', 'true'),
+
+    /**
+     * Sets if the generated file.sql will be deleted after it has been imported.
+     */
+    'removeFileAfterImport' => env('REMOTE_REMOVE_FILE_AFTER_IMPORT', 'true'),
 ];
