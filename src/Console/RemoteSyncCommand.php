7e6ba3c7bf9b210c37703b9eb39e7170d735bce0
Sebastien Pelletier <vesperknight@gmail.com>
2025-11-20 03:11:59 +0200

    Added new --tables= option for syncing only specific tables. Cleaned up output.

--- src/Console/RemoteSyncCommand.php
+++ src/Console/RemoteSyncCommand.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 
 class RemoteSyncCommand extends Command
 {
-    protected $signature   = 'db:remote-sync {--T|test} {--F|filename=}';
+    protected $signature   = 'db:remote-sync {--T|test} {--F|filename=} {--tables=}';
     protected $description = 'Alias of the db:production-sync command.';
 
     public function handle(): bool
@@ -14,6 +14,7 @@ class RemoteSyncCommand extends Command
         return $this->call('db:production-sync', [
             '--test'     => $this->option('test'),
             '--filename' => $this->option('filename'),
+            '--tables'   => $this->option('tables'),
         ]);
     }
 }
